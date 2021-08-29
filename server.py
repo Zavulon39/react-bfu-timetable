91b12407113bea69636f8c42901597454a3674ad
Mikhail <joinmma@gmail.com>
2021-08-29 22:51:53 +0200

    add get_timetable

--- server.py
+++ server.py
@@ -1,6 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.staticfiles import StaticFiles
+from json import load
+from datetime import datetime, timedelta
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+_start: datetime = None
+
+
+def parse(el: dict):
+    for _, institute in el[1].items():
+        for _, directions in institute.items():
+            for item in directions:
+                date = datetime.fromisoformat(item['date'])
+
+                if _start <= date <= date + timedelta(7):
+                    return True
+
+    return False
+
+
+@app.get('/get_timetable/')
+def get_timetable(start: datetime = Query(datetime.now())):
+    global _start
+
+    with open('static/timetable.json', 'r') as file:
+        json = load(file)
+        _start = start
+
+        json = list(filter(parse, json.items()))
+
+        return json
