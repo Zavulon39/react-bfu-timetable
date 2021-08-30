b8d94d9bf18386edd839c64718670c35ae956f1e
Mikhail <joinmma@gmail.com>
2021-08-30 16:27:24 +0200

    upgrade buisness logic

--- server.py
+++ server.py
@@ -2,34 +2,34 @@ from fastapi import FastAPI, Query
 from fastapi.staticfiles import StaticFiles
 from json import load
 from datetime import datetime, timedelta
+from copy import deepcopy
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-_start: datetime = None
-
-
-def parse(el: dict):
-    for _, institute in el[1].items():
-        for _, directions in institute.items():
-            for item in directions:
-                date = datetime.fromisoformat(item['date'])
-
-                if _start <= date <= _start + timedelta(7):
-                    return True
-
-    return False
-
 
 @app.get('/get_timetable/')
 def get_timetable(start: str = Query(str(datetime.now()))):
-    global _start
 
     with open('static/timetable.json', 'r') as file:
         json = load(file)
+        resp = deepcopy(json)
         _start = datetime(*map(int, start.split('T')[0].split('-')))
 
-        json = list(filter(parse, json.items()))
+        for _el, el in resp.items():
+            for _institute, institute in el.items():
+                for _directions, directions in institute.items():
+                    directions.clear()
 
-        return json
+        for _el, el in json.items():
+            for _institute, institute in el.items():
+                for _directions, directions in institute.items():
+                    for item in directions:
+                        date = datetime.fromisoformat(item['date'])
+                        
+
+                        if _start <= date <= _start + timedelta(7):
+                            resp[_el][_institute][_directions].append(item)
+                                
+        return resp
